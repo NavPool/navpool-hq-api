098235e36ff58a46ac505d53576e1d31a56a880a
dantudor <dtudor01@gmail.com>
2019-05-20 13:24:42 +0100

    Remove unused errors

--- service/address/service.go
+++ service/address/service.go
@@ -17,8 +17,6 @@ var (
 	ErrorUnableToFindAddress          = errors.New("Unable to find the address on your account")
 	ErrorUnableToRetrieveTransactions = errors.New("Unable to retrieve transactions")
 	ErrorUnableToDeleteAddress        = errors.New("Unable to delete the address")
-	ErrorInvalidAddress               = errors.New("The address provided is invalid")
-	ErrorColdStakingDetected          = errors.New("The address provided is a cold stakign address")
 	ErrorSpendingAddressAlreadyInUse  = errors.New("The spending address provided is already in use")
 	ErrorUnableToSaveAddress          = errors.New("Unable to save the address")
 )
